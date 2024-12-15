7915408ae084086ee84162b6d43e331c2cf41bae
johnrozmaryn <43191472+johnrozmaryn@users.noreply.github.com>
2024-12-15 08:13:28 -0500

    Day11

---
+++ 2024/day11a.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+f = open("day11.in")
+contents = (f.readlines())
+m = contents[0].split()
+
+stones = defaultdict(int)
+xfer = defaultdict(str)
+
+
+for s in m:
+    stones[s] += 1
+    
+xfer['0'] =['1']
+
+def blink(stones):
+    newstones = defaultdict(int)
+    for item in stones:
+        if xfer[item] == '':
+            if len(item) % 2 == 0:
+                l = len(item)
+                mid = int(l/2)
+                left = item[:mid]
+                right = item[mid:]
+                xfer[item] = [str(int(left)),str(int(right))]
+            else:
+                xfer[item]= [str(2024 * int(item))]          
+
+        for i in xfer[item]:
+            newstones[i] += stones[item]
+    return newstones
+print(stones)
+for i in range(75):
+    stones = blink(stones)
+tot = 0    
+for s in stones:
+    tot += stones[s]
+print(tot)
+
+            
+           
+            
+
+                
+                
+
+            
+        
+    
+
